550aab1325e0181d8ef71383367545f92e997317
Tiago Ferreira <tiago.ferreira@2mi.com.br>
2020-04-16 00:34:44 -0300

    Concluido aulas Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import pandas as pd 
+
 
 def main():
+    df = pd.read_csv('iris.csv')
     st.title("hello")
     st.header('This is a header')
     st.subheader('This is a subheader')
@@ -8,6 +11,18 @@ def main():
     variavel = st.text_input('Texto')
     st.text(variavel)
     st.sidebar.text("Sidebar na área")
-
+    botton = st.button("Clica ai porra")
+    if botton:
+        st.text("clicou sim.")
+    check = st.checkbox('Checkbox')
+    if check:
+        st.markdown('checkado')
+    radio = st.radio('Escolha as opções',('opt 1','opt 2'))
+    if radio == 'opt 1':
+        st.markdown('opção 01')
+    else:
+        st.markdown('opção 02')
+    slider = st.sidebar.slider("Tamanho Visual",min_value=1,max_value=df.shape[0])
+    st.dataframe(df.head(slider))
 if __name__ == '__main__':
     main()
